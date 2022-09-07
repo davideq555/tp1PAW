01778e1ca9136b4c9c39bdb794b3696219a5852f
davideq555 <davideq555@gmail.com>
2022-09-07 17:48:27 -0300

    calcular vocales

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,40 @@
 
-function holaMundo(){
-    alert ("hola mundo");
-    let elem = document.getElementById("");
-}
+//document.getElementById("name").addEventListener("keydown", setName());
+/*let now = new Date();
+document.getElementById("fechaActual").valueAsDate = new Date();
+// function setName(){
+//     let campo = document.getElementById("campo");
+//     console.log(campo);
+//     campo.innerHTML = campo.innerHTML + event.key;
+// }
+/*
+function habilitarCategoria(){
+    let campo = document.getElementById("persona");
+    let selected = campo.options[campo.selectedIndex].text;
+    if (selected == "Persona"){
+        document.getElementById("masInfo").style.visibility = "visible";
+    } else {
+        document.getElementById("masInfo").style.visibility = "hidden";
+    }
+}*/
 
-document.getElementById("name").addEventListener("keydown", setName());
-function setName(){
-    let campo = document.getElementById("campo");
-    console.log(campo);
-    campo.innerHTML = campo.innerHTML + event.key;
+function calcularLetras(){
+    let oracion = document.getElementById("oracion");
+    let cant= document.getElementById("cant");
+    let c = 0;
+    let str = oracion.value;
+    let countVowels = (str) => (str.match(/[aeiou]/gi) || []).length;
+   /* for (let i=0; i < oracion.value.length; i++){
+
+        if (oracion.value[i]=="a"||
+        oracion.value[i]=="e"||
+        oracion.value[i]=="i"||
+        oracion.value[i]=="o"||
+        oracion.value[i]=="u"){
+            c++
+        }
+    }*/
+    console.log(countVowels);
+    cant.value = countVowels;
+    return false;
 }
